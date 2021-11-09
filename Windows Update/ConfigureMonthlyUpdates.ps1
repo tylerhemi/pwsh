7bade67777e7a8d0cedd4b45fe0f2a61894f2ade
Tyler Hemingsen <tyler@hemielectric.com>
2021-11-08 18:47:11 -0800

    Added job to update

--- Windows Update/ConfigureMonthlyUpdates.ps1
+++ Windows Update/ConfigureMonthlyUpdates.ps1
@@ -1,17 +1,24 @@
-        Try{
-            Import-Module NuGet -ErrorAction Stop
-        }
-        catch [System.IO.FileNotFoundException]{
-                Install-PackageProvider -Name NuGet -Force | Out-Null
-                Install-Module Nuget -Force | Out-Null
-                $Error.Clear()
-        }    
-                       
-        #Check for PSWindowsUpdate Module
-        Try{
-            Import-Module PSWindowsUpdate -ErrorAction Stop
-        }
-        catch [System.IO.FileNotFoundException]{
-                Install-Module PSWindowsUpdate -Force | Out-Null
-                $Error.Clear()
-        }
+Try{
+        Import-Module NuGet -ErrorAction Stop
+}
+catch [System.IO.FileNotFoundException]{
+        Install-PackageProvider -Name NuGet -Force | Out-Null
+        Install-Module Nuget -Force | Out-Null
+        $Error.Clear()
+}    
+                
+#Check for PSWindowsUpdate Module
+Try{
+        Import-Module PSWindowsUpdate -ErrorAction Stop
+}
+catch [System.IO.FileNotFoundException]{
+        Install-Module PSWindowsUpdate -Force | Out-Null
+        $Error.Clear()
+}
+
+$JobTrigger = @{
+        Frequency = "Weekly"
+        At = "1:00AM"
+        DaysOfWeek = "Wednesday"
+}
+Register-ScheduledJob -Name "Weekly Updates" -ScriptBlock{Install-WindowsUpdate -AcceptAll -Install -Confirm:$false  -AutoReboot:$true} -Trigger $JobTrigger
