38a34b9eda98d7b27aa69ec361be71b08b92f979
tylerhemi <92611466+tylerhemi@users.noreply.github.com>
2021-11-23 19:48:31 -0800

    Update SSHConfig.ps1
    Taken from website

--- HostConfiguration/SSHConfig.ps1
+++ HostConfiguration/SSHConfig.ps1
@@ -1,4 +1,10 @@
+#https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+#
+
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
 
 # Start the sshd service
 Start-Service sshd
@@ -6,9 +12,10 @@ Start-Service sshd
 # OPTIONAL but recommended:
 Set-Service -Name sshd -StartupType 'Automatic'
 
-# Confirm the firewall rule is configured. It should be created automatically by setup.
-Get-NetFirewallRule -Name *ssh*
-
-# There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled
-# If the firewall does not exist, create one
-New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
